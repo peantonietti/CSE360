b3fab1f40f1c5065fa207d455a9e47bc03188cf3
Peter Antonietti <peantonietti@gmail.com>
2019-10-09 22:25:09 -0700

    first commit

---
+++ AddingMachine.java
@@ -0,0 +1,30 @@
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () { //getTotal method should return the current total
+		return 0;
+	}
+	
+	public void add (int value) { //add the parameter to the total variable
+		
+	}
+	
+	public void subtract (int value) { //subtract the parameter from the total variable
+		
+	}
+		
+	public String toString () { //return history of transactions
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
