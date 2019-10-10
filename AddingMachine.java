5c14d54aaaaba03258163b618cbfeaf6dcfbf73c
Peter Antonietti <peantonietti@gmail.com>
2019-10-09 22:54:41 -0700

    fleshed out methods

--- AddingMachine.java
+++ AddingMachine.java
@@ -3,25 +3,36 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history;
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		
+		history = "0";
 	}
 	
 	public int getTotal () { //getTotal method should return the current total
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) { //add the parameter to the total variable
+		
+		total = total + value;
+		
+		history = history + " + "+value;
 		
 	}
 	
 	public void subtract (int value) { //subtract the parameter from the total variable
 		
+		total = total - value;
+		
+		history = history + " - "+value;
+		
 	}
 		
 	public String toString () { //return history of transactions
-		return "";
+		return history;
 	}
 
 	public void clear() {
